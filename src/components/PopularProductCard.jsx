bc2fb3a5b9fd73a0469aa1d529d8b90ae57b75ce
faizan3165 <faizanmehmood3165@gmail.com>
2024-01-30 04:49:27 +0500

    complete super quality section

--- src/components/PopularProductCard.jsx
+++ src/components/PopularProductCard.jsx
@@ -13,9 +13,13 @@ const PopularProductCard = ({ imgURL, name, price }) => {
         </p>
       </div>
 
-      <h3 className="mt-2 text-2xl leading-normal font-semibold font-palanquin">{name}</h3>
-      
-      <p className="mt-2 font-semibold font-montserrat text-coral-red text-2xl leading-normal">{price}</p>
+      <h3 className="mt-2 text-2xl leading-normal font-semibold font-palanquin">
+        {name}
+      </h3>
+
+      <p className="mt-2 font-semibold font-montserrat text-coral-red text-2xl leading-normal">
+        {price}
+      </p>
     </div>
   );
 };
