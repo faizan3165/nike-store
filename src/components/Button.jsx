906e71be14437556c6aa452747b5dd5a02ad87a6
faizan3165 <faizanmehmood3165@gmail.com>
2024-01-30 04:18:50 +0500

    complete hero section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
 
-const Button = () => {
+const Button = ({ label, iconUrl }) => {
   return (
-    <div>Button</div>
-  )
-}
+    <button className="flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red rounded-full text-white border-coral-red">
+      {label}
 
-export default Button
+      <img src={iconUrl} alt={label} className="ml-2 rounded-full w-5 h-5" />
+    </button>
+  );
+};
+
+export default Button;
--- src/components/ShoeCard.jsx
+++ src/components/ShoeCard.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from "react";
 
-const ShoeCard = () => {
+const ShoeCard = ({ imgUrl, changeBannerImg, bannerImg }) => {
+  const handleClick = () => {
+    if (bannerImg != imgUrl.bigShoe) {
+      changeBannerImg(imgUrl.bigShoe);
+    }
+  };
+
   return (
-    <div>ShoeCard</div>
-  )
-}
+    <div
+      className={`border-2 rounded-xl ${
+        bannerImg === imgUrl.bigShoe ? "border-coral-red" : "border-transparent"
+      } cursor-pointer max-sm:flex-1`}
+      onClick={handleClick}
+    >
+      <div className="flex justify-center items-center bg-card bg-center bg-cover sm:w-40 sm:h-40 rounded-xl max-sm:p-4">
+        <img
+          src={imgUrl.thumbnail}
+          alt="shoe"
+          className="object-contain"
+          width={127}
+          height={103}
+        />
+      </div>
+    </div>
+  );
+};
 
-export default ShoeCard
+export default ShoeCard;
